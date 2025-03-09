fc76de63b2e2b04728f1279e201bd6ac835a7c05
G10-ria <gloriamwenga41@gmail.com>
2025-03-09 22:30:15 +0300

    Create Bankmanagement.cpp
    This c++ program demonstrates multi-level inheritance involving bank management system.

---
+++ Bankmanagement.cpp
@@ -0,0 +1,63 @@
+/*c++ function
+Author
+Ren no
+Date
+version 1
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Base Class: AccountHolder
+class AccountHolder {
+protected:
+    string name;
+public:
+    void setName(string n) { 
+      name = n; }
+    string getName() { 
+      return name; }
+};
+
+// Derived Class: BankAccount (inherits from AccountHolder)
+class BankAccount : public AccountHolder {
+private:
+    int accountNumber;
+    double balance;
+public:
+    BankAccount(string n, int accNum, double bal) {
+        name = n;
+        accountNumber = accNum;
+        balance = bal;
+    }
+    int getAccountNumber() { 
+      return accountNumber; }
+    double getBalance() { 
+      return balance; }
+};
+
+// Derived Class: SavingsAccount (inherits from BankAccount)
+class SavingsAccount : public BankAccount {
+private:
+    double interestRate;
+public:
+    SavingsAccount(string n, int accNum, double bal, double rate)
+        : BankAccount(n, accNum, bal), interestRate(rate) {}
+
+    double getInterestRate() {
+      return interestRate; }
+};
+
+// Main function to test the classes
+int main() {
+    SavingsAccount myAccount("Gloria", 12345, 4000.75, 1.6);
+
+    cout << "Account Holder: " << myAccount.getName() << endl;
+    cout << "Account Number: " << myAccount.getAccountNumber() << endl;
+    cout << "Balance: $" << myAccount.getBalance() << endl;
+    cout << "Interest Rate: " << myAccount.getInterestRate() << "%" << endl;
+
+    return 0;
+}
+
